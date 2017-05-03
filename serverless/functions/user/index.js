81e16831b84af7e62f90582b0d8deaffe82a2295
Sean <seanpapanikolas@gmail.com>
2017-05-02 22:30:22 -0700

    comment out delete user data

--- serverless/functions/user/index.js
+++ serverless/functions/user/index.js
@@ -6,7 +6,7 @@ const getUserData = require('./read/getUserData');
 const updateUserPassword = require('./update/updateUserPassword');
 const updateUserEmail = require('./update/updateUserEmail');
 const updateUserProfile = require('./update/updateUserProfile');
-const deleteUserData = require('./delete/deleteData');
+// const deleteUserData = require('./delete/deleteData');
 
 module.exports = {
   createUser: create.createUser,
@@ -16,5 +16,5 @@ module.exports = {
   updateUserEmail,
   getUserData,
   login,
-  deleteUserData,
+  // deleteUserData,
 };
