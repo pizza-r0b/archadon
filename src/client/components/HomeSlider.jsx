3f6fce54423d50338f1e82c95bf01a6c6d925847
Sean <seanpapanikolas@gmail.com>
2017-09-18 23:30:09 -0700

    adding more slider animation

--- src/client/components/HomeSlider.jsx
+++ src/client/components/HomeSlider.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { IMAGE_ORIGIN } from 'Constants';
+import { CSSTransitionGroup } from 'react-transition-group';
 
 // Name: 3'1x5'1 | Width: | Height: 5.1 | Price: 344.74 | Qty: 1
 // Short Description: 100 knot
@@ -24,48 +25,122 @@ import { IMAGE_ORIGIN } from 'Constants';
 const images = [
   {
     headline: 'Headline 1',
+    i: 0,
     title: 'Soumak Weave',
     subtitle: 'One of a kind',
     image: `${IMAGE_ORIGIN}/3.1x5.1_32438_29431.jpg`,
     id: '59bdbf4f857c5b78b3a4c440',
   },
   {
-    headline: 'Headline 1',
+    headline: 'Headline 2',
+    i: 1,
     title: 'Title',
     subtitle: 'Sub Title',
     image: `${IMAGE_ORIGIN}/6x9_S_2081_50.jpg`,
     id: '59bdbf4f857c5b78b3a4c4f0',
   },
   {
-    headline: 'Headline 1',
+    headline: 'Headline 3',
+    i: 2,
     title: 'Title',
     subtitle: 'Sub Title',
     image: `${IMAGE_ORIGIN}/6.1x9.1_S_2090_59.jpg`,
     id: '59bdbf4f857c5b78b3a4c49d',
   },
   {
-    headline: 'Headline 1',
+    headline: 'Headline 4',
+    i: 3,
     title: 'Title',
     subtitle: 'Sub Title',
     image: `${IMAGE_ORIGIN}/6x9_32963_29737.jpg`,
     id: '59bdbf4f857c5b78b3a4c4ee',
   },
-]
+];
+
 
 class HomeSlider extends React.Component {
+  state = {
+    current: 0,
+    headlines: images.filter((img, i) => i !== 0),
+    line: true,
+  }
+
+  interval = (callback, delay) => {
+    const tick = now => {
+      if (now - start >= delay) {
+        start = now;
+        callback();
+      }
+      requestAnimationFrame(tick);
+    };
+    let start = performance.now();
+    return requestAnimationFrame(tick);
+  }
+
+  getHeadlines = (current) => images.filter((img, i) => i !== current)
+
+  updateState = (current, cb) => {
+    this.setState({
+      headlines: this.getHeadlines(current),
+      current,
+    }, cb);
+  }
+
+  startInterval = () => {
+    this.interval = this.interval(() => {
+      const current = this.state.current === images.length - 1 ? 0 : this.state.current + 1;
+      this.line.classList.remove('grow');
+      this.updateState(current, () => {
+        setTimeout(() => {
+          this.line.classList.add('grow');
+        }, 25);
+      });
+    }, 8000);
+    setTimeout(() => {
+      this.line.classList.add('grow');
+    }, 25);
+  }
+
+  componentDidMount() {
+    this.startInterval();
+  }
+
+  headlineClick = i => () => {
+    this.line.classList.remove('grow');
+    this.updateState(i, this.startInterval);
+  }
+
   render() {
     return (
       <div>
         <div className="home-slider">
           <div className="home-slider-item">
             <div className="headlines">
-              <h1>{images[0].headline}</h1>
+              <CSSTransitionGroup
+                transitionName="homeslider-headline-transition"
+                transitionEnterTimeout={920}
+                transitionLeaveTimeout={920}
+                component="div"
+                className="trans-wrap"
+              >
+                <h1 className="main-headline" key={images[this.state.current].id}>{images[this.state.current].headline}</h1>
+              </CSSTransitionGroup>
               <div className="line">
-                <div className="line--inner" />
+                <div ref={c => { this.line = c; }} className='line--inner' />
               </div>
-              <div className="headlines--link">{images[1].headline}</div>
-              <div className="headlines--link">{images[2].headline}</div>
-              <div className="headlines--link">{images[3].headline}</div>
+              <CSSTransitionGroup
+                transitionName="homeslider-headline-transition"
+                transitionEnterTimeout={920}
+                transitionLeaveTimeout={920}
+                className="trans-wrap headline-links"
+              >
+                <div key={this.state.current} className="headline-links-wrap">
+                  {this.state.headlines.map((item) => (
+                    <div onClick={this.headlineClick(item.i)} className="headline-link">{item.headline}</div>
+                  ))}
+                </div>
+
+              </CSSTransitionGroup>
             </div>
           </div>
           <div className="featured-img img home-slider-item">
