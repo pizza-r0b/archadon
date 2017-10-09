9ac778f474e70953825d696c792fc29209583a9c
Sean <seanpapanikolas@gmail.com>
2017-10-08 18:24:41 -0700

    dev deploy

---
+++ webpack/server.deploy.js
@@ -0,0 +1,42 @@
+const nodeExternals = require('webpack-node-externals');
+const webpack = require('webpack');
+const base = require('./base.js');
+const Dotenv = require('dotenv-webpack');
+const path = require('path');
+
+const serverConfig = base({
+  rules: [
+    {
+      test: /\.scss$/,
+      use: ['css-loader', {
+        loader: 'sass-loader',
+        options: {
+          data: '$env: ' + process.env.NODE_ENV + ';',
+        },
+      }],
+    },
+  ],
+  config: {
+    entry: {
+      server: './src/server/index.js',
+    },
+    target: 'node',
+    plugins: [
+      new Dotenv({
+        path: path.resolve(__dirname, '../.env_dev'),
+      }),
+      new webpack.DefinePlugin({
+        'process.env': {
+          NODE_ENV: JSON.stringify('development'),
+        },
+      }),
+      new webpack.optimize.UglifyJsPlugin(),
+      new webpack.BannerPlugin({ banner: 'require("source-map-support").install();', raw: true, entryOnly: false }),
+    ],
+    externals: [nodeExternals({
+      whitelist: ['react-hot-loader'],
+    })],
+  },
+});
+
+module.exports = serverConfig;
