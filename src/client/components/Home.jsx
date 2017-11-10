ee6adc318c9f0133b49c1e17776f7cdc149166ce
Sean <seanpapanikolas@gmail.com>
2017-11-10 13:01:36 -0800

    adding christmas module

--- src/client/components/Home.jsx
+++ src/client/components/Home.jsx
@@ -4,6 +4,10 @@ import HomeFeaturedItems from './HomeFeaturedBoxes';
 import ProductList from './ProductList';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { IMAGE_ORIGIN } from 'Constants';
+
+const returnSrcSet = (sku, ext = 'jpg') => `${IMAGE_ORIGIN}/sm_${sku}.${ext} 329w, ${IMAGE_ORIGIN}/md_${sku}.${ext} 658w, ${IMAGE_ORIGIN}/lg_${sku}.${ext} 1315w`;
+
 
 class Home extends React.Component {
   render() {
@@ -19,6 +23,17 @@ class Home extends React.Component {
           <div className="margin--bottom-10" style={{ width: '100%', maxWidth: '650px', margin: 'auto' }}>
             <h2 className="align--center">Hello there, and welcome!</h2>
             <p className="align--center">We’ve rolled out the welcome mat, so stay awhile. Browse our extensive collection of heirloom-quality rugs, save your faves, and imagine how a stunning rug will tie the room together.</p>
+          </div>
+
+          <div className="flex-parent flex-col-break hp-christmas margin--bottom-10">
+          <div className="align--center margin--bottom-5">
+            <Link to="/shop?tid=hp2"><img srcSet={returnSrcSet('christmas-rugs')} /></Link>
+          </div>
+          <div>
+            <h1>Find the perfect rug to tie their room together this holiday season.</h1>
+            <h1 className="font-color--lighter">We won't tell if you get one for you too.</h1>
+            <Link className="btn--alt margin--top-5" to="/shop?tid=hp1">Find Rugs You Love</Link>
+          </div>
           </div>
 
           <HomeFeaturedItems />
