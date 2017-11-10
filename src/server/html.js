c8d01316d5dce07e4d72c1caa46a125c11951568
Sean <seanpapanikolas@gmail.com>
2017-11-10 12:31:22 -0800

    adding fb pixel

--- src/server/html.js
+++ src/server/html.js
@@ -14,6 +14,26 @@ const ga = process.env.NODE_ENV !== 'production' ? '' : (
 </script>`
 );
 
+const fb = process.env.NODE_ENV !== 'production' ? '' : (
+    `
+    <!-- Facebook Pixel Code -->
+    <script>
+    !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+    n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+    document,'script','https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '498852017156109');
+    fbq('track', 'PageView');
+    </script>
+    <noscript><img height="1" width="1" style="display:none"
+    src="https://www.facebook.com/tr?id=498852017156109&ev=PageView&noscript=1"
+    /></noscript>
+    <!-- DO NOT MODIFY -->
+    <!-- End Facebook Pixel Code -->
+    `
+);
+
 const css = readFileSync(resolve(resolve(), './public/styles.css'));
 const vendor = readFileSync(resolve(resolve(), './public/vendor.js'));
 
@@ -38,6 +58,7 @@ export default function html(routerWithContext, props) {
                   ''
               }
               ${ga}
+              ${fb}
           </head>
           <body>
               <div id="app">${routerWithContext}</div>
